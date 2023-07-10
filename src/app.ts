0459f800cab65717f73bcec71acf96778cacdeb4
chubbystrings <martinsokwor@hotmail.com>
2023-07-10 13:10:53 +0100

    vercel deploy

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ import rolesRouter from './routes/roles';
 const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, "../", 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, "../", 'views'));
+app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
